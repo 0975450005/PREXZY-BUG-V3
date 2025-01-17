cd9aaf309ff6042032098f44ac7adc8dd40ed286
0975450005 <131523856+0975450005@users.noreply.github.com>
2025-01-17 12:28:58 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '2347063956321'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '‪918974034453‬'  //creator number
+global.ownername = 'Queen priya' //owner name
 global.botname = '𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻3' //name of the bot
 
 //sticker details
